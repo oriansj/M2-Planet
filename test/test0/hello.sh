4916fb9385a86d5aca37e9f608c215ea90cc2256
Jeremiah Orians <jeremiah@pdp10.guru>
2018-01-20 18:33:11 -0500

    Put common x86 platform logic in a shared folder to reduce duplicate code

--- test/test0/hello.sh
+++ test/test0/hello.sh
@@ -3,9 +3,9 @@ set -ex
 # Build the test
 bin/M2-Planet test/test0/cc500.c test/test0/cc0.M1 || exit 1
 # Macro assemble with libc written in M1-Macro
-M1 -f test/test0/x86_defs.M1 -f test/test0/libc.M1 -f test/test0/cc0.M1 --LittleEndian --Architecture 1 -o test/test0/cc0.hex2 || exit 2
+M1 -f test/common_x86/x86_defs.M1 -f test/common_x86/libc.M1 -f test/test0/cc0.M1 --LittleEndian --Architecture 1 -o test/test0/cc0.hex2 || exit 2
 # Resolve all linkages
-hex2 -f test/test0/ELF-i386.hex2 -f test/test0/cc0.hex2 --LittleEndian --Architecture 1 --BaseAddress 0x8048000 -o test/results/test0-binary --exec_enable || exit 3
+hex2 -f test/common_x86/ELF-i386.hex2 -f test/test0/cc0.hex2 --LittleEndian --Architecture 1 --BaseAddress 0x8048000 -o test/results/test0-binary --exec_enable || exit 3
 
 # Ensure binary works if host machine supports test
 if [ "$(get_machine)" = "x86_64" ]
--- test/test1/hello.sh
+++ test/test1/hello.sh
@@ -3,9 +3,9 @@ set -ex
 # Build the test
 bin/M2-Planet test/test1/for.c test/test1/for.M1 || exit 1
 # Macro assemble with libc written in M1-Macro
-M1 -f test/test1/x86_defs.M1 -f test/test1/libc.M1 -f test/test1/for.M1 --LittleEndian --Architecture 1 -o test/test1/for.hex2 || exit 2
+M1 -f test/common_x86/x86_defs.M1 -f test/common_x86/libc.M1 -f test/test1/for.M1 --LittleEndian --Architecture 1 -o test/test1/for.hex2 || exit 2
 # Resolve all linkages
-hex2 -f test/test1/ELF-i386.hex2 -f test/test1/for.hex2 --LittleEndian --Architecture 1 --BaseAddress 0x8048000 -o test/results/test1-binary --exec_enable || exit 3
+hex2 -f test/common_x86/ELF-i386.hex2 -f test/test1/for.hex2 --LittleEndian --Architecture 1 --BaseAddress 0x8048000 -o test/results/test1-binary --exec_enable || exit 3
 
 # Ensure binary works if host machine supports test
 if [ "$(get_machine)" = "x86_64" ]
