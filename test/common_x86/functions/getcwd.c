f77e0ed6cb007ab391fe95208f3ed13d4575d505
Jeremiah Orians <jeremiah@pdp10.guru>
2020-01-25 16:24:12 -0500

    A quick prototype for getcwd

---
+++ test/common_x86/functions/getcwd.c
@@ -0,0 +1,40 @@
+/* Copyright (C) 2020 Jeremiah Orians
+ * This file is part of M2-Planet.
+ *
+ * M2-Planet is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * M2-Planet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with M2-Planet.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include<stdlib.h>
+//CONSTANT PATH_MAX 4096
+#define PATH_MAX 4096
+
+char* getcwd(char* buf, size_t size)
+{
+	asm("LOAD_EFFECTIVE_ADDRESS_ebx %8"
+	"LOAD_INTEGER_ebx"
+	"LOAD_EFFECTIVE_ADDRESS_ecx %4"
+	"LOAD_INTEGER_ecx"
+	"LOAD_IMMEDIATE_eax %183"
+	"INT_80");
+}
+
+char* getwd(char* buf)
+{
+	return getcwd(buf, PATH_MAX);
+}
+
+char* get_current_dir_name()
+{
+	return getcwd(malloc(PATH_MAX), PATH_MAX);
+}
