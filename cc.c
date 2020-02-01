71062011cda7beb17dc1af3049e615f795dbbf5c
Jeremiah Orians <jeremiah@pdp10.guru>
2020-02-01 16:21:10 -0500

    Release 1.5.0

--- cc.c
+++ cc.c
@@ -100,7 +100,7 @@ int main(int argc, char** argv)
 		}
 		else if(match(argv[i], "-V") || match(argv[i], "--version"))
 		{
-			file_print("M2-Planet v1.3.0\n", stderr);
+			file_print("M2-Planet v1.5.0\n", stderr);
 			exit(EXIT_SUCCESS);
 		}
 		else
