89c6513cd81a92e20f6d067a6c76e60741421567
Sanne Wouda <sanne.wouda@gmail.com>
2021-01-05 20:17:25 +0000

    Add support for conditional inclusion
    - support for #if, #elif, #else, #endif, but no macro expansion just
      yet. Any macro is treated as undefined and expands to 0.
    
    for example:
    ./bin/M2-Planet -E test.c
    
     #if 1 == FOO
    foo
     #else
    bar
     #endif
    
    results in
    
    bar

--- cc_macro.c
+++ cc_macro.c
@@ -18,6 +18,15 @@
 #include "gcc_req.h"
 
 void require(int bool, char* error);
+int numerate_string(char *a);
+
+struct conditional_inclusion {
+    struct conditional_inclusion* prev;
+    int include; /* 1 == include, 0 == skip */
+    int previous_condition_matched; /* 1 == all subsequent conditions treated as FALSE */
+};
+
+struct conditional_inclusion *conditional_inclusion_top;
 
 /* point where we are currently modifying the global_token list */
 struct token_list* macro_token;
@@ -56,18 +65,230 @@ void eat_newline_tokens()
     }
 }
 
-void macro_directive()
+int macro_expression();
+int macro_variable()
 {
-    /* unhandled macro directive; let's eat until a newline; om nom nom */
-    while (TRUE)
+    eat_current_token();
+    return 0;
+}
+int macro_number()
+{
+    int result = numerate_string(macro_token->s);
+    eat_current_token();
+    return result;
+}
+
+int macro_primary_expr()
+{
+    if ('-' == macro_token->s[0])
     {
         eat_current_token();
+        return -macro_primary_expr();
+    }
+    else if ('!' == macro_token->s[0])
+    {
+        eat_current_token();
+        return !macro_primary_expr();
+    }
+    else if ('(' == macro_token->s[0])
+    {
+        eat_current_token();
+        return macro_expression();
+    } else if (in_set(macro_token->s[0], "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_"))
+        return macro_variable();
+    else if (in_set(macro_token->s[0], "0123456789"))
+        return macro_number();
+    else
+        return 0; /* FIXME: error handling */
+}
 
-        if (NULL == macro_token)
-            return;
+int macro_additive_expr()
+{
+    int lhs = macro_primary_expr();
 
-        if ('\n' == macro_token->s[0])
-            return;
+    if (match("+", macro_token->s))
+    {
+        eat_current_token();
+        return lhs + macro_additive_expr();
+    }
+    else if (match("-", macro_token->s))
+    {
+        eat_current_token();
+        return lhs - macro_additive_expr();
+    }
+    else if (match("*", macro_token->s))
+    {
+        eat_current_token();
+        return lhs * macro_additive_expr();
+    }
+    else if (match("/", macro_token->s))
+    {
+        eat_current_token();
+        return lhs / macro_additive_expr();
+    }
+    else if (match("%", macro_token->s))
+    {
+        eat_current_token();
+        return lhs % macro_additive_expr();
+    }
+    else if (match(">>", macro_token->s))
+    {
+        eat_current_token();
+        return lhs >> macro_additive_expr();
+    }
+    else if (match("<<", macro_token->s))
+    {
+        eat_current_token();
+        return lhs << macro_additive_expr();
+    }
+    else
+    {
+        return lhs;
+    }
+}
+
+int macro_relational_expr()
+{
+    int lhs = macro_additive_expr();
+
+    if (match("<", macro_token->s))
+    {
+        eat_current_token();
+        return lhs < macro_relational_expr();
+    }
+    else if (match("<=", macro_token->s))
+    {
+        eat_current_token();
+        return lhs <= macro_relational_expr();
+    }
+    else if (match(">=", macro_token->s))
+    {
+        eat_current_token();
+        return lhs >= macro_relational_expr();
+    }
+    else if (match(">", macro_token->s))
+    {
+        eat_current_token();
+        return lhs > macro_relational_expr();
+    }
+    else if (match("==", macro_token->s))
+    {
+        eat_current_token();
+        return lhs == macro_relational_expr();
+    }
+    else if (match("!=", macro_token->s))
+    {
+        eat_current_token();
+        return lhs != macro_relational_expr();
+    }
+    else
+    {
+        return lhs;
+    }
+}
+
+int macro_bitwise_expr()
+{
+    int rhs;
+    int lhs = macro_relational_expr();
+
+    if (match("&", macro_token->s))
+    {
+        eat_current_token();
+        return lhs & macro_bitwise_expr();
+    }
+    else if (match("&&", macro_token->s))
+    {
+        eat_current_token();
+        rhs = macro_bitwise_expr();
+        return lhs && rhs;
+    }
+    else if (match("|", macro_token->s))
+    {
+        eat_current_token();
+        rhs = macro_bitwise_expr();
+        return lhs | rhs;
+    }
+    else if (match("||", macro_token->s))
+    {
+        eat_current_token();
+        rhs = macro_bitwise_expr();
+        return lhs || rhs;
+    }
+    else if (match("^", macro_token->s))
+    {
+        eat_current_token();
+        rhs = macro_bitwise_expr();
+        return lhs ^ rhs;
+    }
+    else
+    {
+        return lhs;
+    }
+}
+
+int macro_expression()
+{
+    return macro_bitwise_expr();
+}
+
+
+
+void macro_directive()
+{
+    struct conditional_inclusion *t;
+    int result;
+    /* FIXME: whitespace is allowed between "#"" and "if" */
+    if(match("#if", macro_token->s))
+    {
+        eat_current_token();
+        /* evaluate constant integer expression */
+        result = macro_expression();
+        /* push conditional inclusion */
+        t = calloc(1, sizeof(struct conditional_inclusion));
+        t->prev = conditional_inclusion_top;
+        conditional_inclusion_top = t;
+        t->include = TRUE;
+        if (FALSE == result)
+        {
+            t->include = FALSE;
+        }
+        t->previous_condition_matched = t->include;
+    }
+    else if (match("#elif", macro_token->s))
+    {
+        eat_current_token();
+        result = macro_expression();
+        conditional_inclusion_top->include = result && !conditional_inclusion_top->previous_condition_matched;
+        conditional_inclusion_top->previous_condition_matched =
+            conditional_inclusion_top->previous_condition_matched || conditional_inclusion_top->include;
+    }
+    else if (match("#else", macro_token->s))
+    {
+        eat_current_token();
+        conditional_inclusion_top->include = !conditional_inclusion_top->previous_condition_matched;
+    }
+    else if (match("#endif", macro_token->s))
+    {
+        eat_current_token();
+        /* pop conditional inclusion */
+        t = conditional_inclusion_top;
+        conditional_inclusion_top = conditional_inclusion_top->prev;
+        free(t);
+    }
+    else
+    {
+        /* unhandled macro directive; let's eat until a newline; om nom nom */
+        while (TRUE)
+        {
+            if (NULL == macro_token)
+                return;
+
+            if ('\n' == macro_token->s[0])
+                return;
+
+            eat_current_token();
+        }
     }
 }
 
@@ -93,7 +314,19 @@ void preprocess()
         else
         {
             start_of_line = FALSE;
-            macro_token = macro_token->next;
+            if (NULL == conditional_inclusion_top)
+            {
+                macro_token = macro_token->next;
+            }
+            else if (!conditional_inclusion_top->include)
+            {
+                /* rewrite the token stream to exclude the current token */
+                eat_current_token();
+            }
+            else
+            {
+                macro_token = macro_token->next;
+            }
         }
     }
 }
