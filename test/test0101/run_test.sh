823be938750b909656799bdb4778a636c6acad8e
Andrius Štikonas <andrius@stikonas.eu>
2021-12-03 21:33:55 +0000

    Enable riscv32 tests.

--- test/test0101/run_test.sh
+++ test/test0101/run_test.sh
@@ -29,6 +29,7 @@ mkdir -p ${TMPDIR}
 	--architecture ${ARCH} \
 	-f M2libc/sys/types.h \
 	-f M2libc/stddef.h \
+	-f M2libc/signal.h \
 	-f M2libc/${ARCH}/Linux/unistd.h \
 	-f M2libc/${ARCH}/Linux/fcntl.h \
 	-f M2libc/${ARCH}/Linux/sys/stat.h \
