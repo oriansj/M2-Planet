195df98ab65bf1d45563bf5a553c196b58c78c31
Andrius Štikonas <andrius@stikonas.eu>
2025-03-16 19:00:43 +0000

    Bump version to 1.12.1.

--- cc.c
+++ cc.c
@@ -51,7 +51,7 @@ int main(int argc, char** argv)
 	 * since cc_* can't handle string constants. */
 	char* m2_major = "1";
 	char* m2_minor = "12";
-	char* m2_patch = "0";
+	char* m2_patch = "1";
 
 	init_macro_env("__M2__", "42", "__INTERNAL_M2__", 0); /* Setup __M2__ */
 	init_macro_env("__M2C__", m2_major, "__INTERNAL_M2__", 0);
