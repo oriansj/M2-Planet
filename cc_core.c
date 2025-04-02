7495ba33d02cb166c3313a3ffed2f32568226633
Gtker <github@gtker.com>
2025-04-02 16:21:21 +0200

    Add write_add function

--- cc_core.c
+++ cc_core.c
@@ -411,67 +411,74 @@ void emit_load_immediate(int reg, int value, char* note)
 }
 
 /* Adds destination and source and places result in destination */
-void emit_add(int destination_reg, int source_reg, char* note)
+void write_add(int destination_reg, int source_reg, char* note)
 {
 	char* destination_name = register_from_string(destination_reg);
 	char* source_name = register_from_string(source_reg);
 
 	if((KNIGHT_POSIX == Architecture) || (KNIGHT_NATIVE == Architecture))
 	{
-		emit_out("ADD R");
-		emit_out(destination_name);
-		emit_out(" R");
-		emit_out(destination_name);
-		emit_out(" R");
-		emit_out(source_name);
+		emit_to_string("ADD R");
+		emit_to_string(destination_name);
+		emit_to_string(" R");
+		emit_to_string(destination_name);
+		emit_to_string(" R");
+		emit_to_string(source_name);
 	}
 	else if(X86 == Architecture || AMD64 == Architecture)
 	{
-		emit_out("add_");
-		emit_out(destination_name);
-		emit_out(",");
-		emit_out(source_name);
+		emit_to_string("add_");
+		emit_to_string(destination_name);
+		emit_to_string(",");
+		emit_to_string(source_name);
 	}
 	else if(ARMV7L == Architecture)
 	{
-		emit_out("'0' ");
-		emit_out(destination_name);
-		emit_out(" ");
-		emit_out(destination_name);
-		emit_out(" ADD ");
-		emit_out(source_name);
-		emit_out(" ARITH2_ALWAYS");
+		emit_to_string("'0' ");
+		emit_to_string(destination_name);
+		emit_to_string(" ");
+		emit_to_string(destination_name);
+		emit_to_string(" ADD ");
+		emit_to_string(source_name);
+		emit_to_string(" ARITH2_ALWAYS");
 	}
 	else if(AARCH64 == Architecture)
 	{
-		emit_out("ADD_");
-		emit_out(destination_name);
-		emit_out("_");
-		emit_out(source_name);
-		emit_out("_");
-		emit_out(destination_name);
+		emit_to_string("ADD_");
+		emit_to_string(destination_name);
+		emit_to_string("_");
+		emit_to_string(source_name);
+		emit_to_string("_");
+		emit_to_string(destination_name);
 	}
 	else if(RISCV32 == Architecture || RISCV64 == Architecture)
 	{
-		emit_out("rd_");
-		emit_out(destination_name);
-		emit_out(" rs1_");
-		emit_out(source_name);
-		emit_out(" rs2_");
-		emit_out(destination_name);
-		emit_out(" add");
+		emit_to_string("rd_");
+		emit_to_string(destination_name);
+		emit_to_string(" rs1_");
+		emit_to_string(source_name);
+		emit_to_string(" rs2_");
+		emit_to_string(destination_name);
+		emit_to_string(" add");
 	}
 
 	if(note == NULL)
 	{
-		emit_out("\n");
+		emit_to_string("\n");
 	}
 	else
 	{
-		emit_out(" # ");
-		emit_out(note);
-		emit_out("\n");
+		emit_to_string(" # ");
+		emit_to_string(note);
+		emit_to_string("\n");
 	}
+}
+
+void emit_add(int destination_reg, int source_reg, char* note)
+{
+	reset_emit_string();
+	write_add(destination_reg, source_reg, note);
+	emit_out(emit_string);
 }
 
 void emit_add_immediate(int reg, int value, char* note)
