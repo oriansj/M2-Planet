f7d7c779793a59263aa7dadaf4025e1294502d56
Laurent Huberdeau <laurenthuberd@gmail.com>
2025-10-27 10:05:28 -0400

    Extend constant_expressions.c test for *

--- test/run-pass/constant_expressions.c
+++ test/run-pass/constant_expressions.c
@@ -27,6 +27,7 @@ enum {
 	SIZEOF_INT = sizeof(int),
 	SIZEOF_GLOBAL = sizeof(global_int),
 	TEN = SIX + 1 + 1 + 2,
+	TWELVE = THREE * 4,
 	STRING_LENGTH = sizeof("1234567"),
 	CHARACTER = 'A', /* 0x41 */
 	CHARACTER_PLUS = 'A' + 10, /* 0x41 */
@@ -36,6 +37,10 @@ struct SizeTwelve {
 	int z[THREE];
 };
 
+struct SizeTwelveBytes {
+	char z[TWELVE];
+};
+
 int global[THREE + 2];
 
 int main() {
@@ -43,38 +48,38 @@ int main() {
 	static int static_local[THREE];
 
 	if(sizeof(struct SizeTwelve) != 3 * sizeof(int)) return 1;
+	if(sizeof(struct SizeTwelveBytes) != TWELVE) return 2;
 
-	if(SIX != 6) return 2;
-	if(sizeof(int) != SIZEOF_INT) return 3;
+	if(SIX != 6) return 3;
+	if(sizeof(int) != SIZEOF_INT) return 4;
 
-	if(sizeof(THREE) != sizeof(int)) return 4;
+	if(sizeof(THREE) != sizeof(int)) return 5;
 
-	if(SIZEOF_GLOBAL != sizeof(int)) return 5;
-	if(SIZEOF_GLOBAL != sizeof(global_int)) return 6;
+	if(SIZEOF_GLOBAL != sizeof(int)) return 6;
+	if(SIZEOF_GLOBAL != sizeof(global_int)) return 7;
 
 	static int static_int;
-	if(sizeof(static_int) != sizeof(int)) return 7;
+	if(sizeof(static_int) != sizeof(int)) return 8;
 
 	int local_int;
-	if(sizeof(local_int) != sizeof(int)) return 8;
+	if(sizeof(local_int) != sizeof(int)) return 9;
 
-	if(TEN != 10) return 9;
+	if(TEN != 10) return 10;
+	if(TWELVE != 12) return 11;
 
-	if(sizeof(local) != sizeof(int) * 3) return 10;
-	if(sizeof(static_local) != sizeof(int) * 3) return 11;
-	if(sizeof(global) != sizeof(int) * 5) return 12;
+	if(sizeof(local) != sizeof(int) * 3) return 12;
+	if(sizeof(static_local) != sizeof(int) * 3) return 13;
+	if(sizeof(global) != sizeof(int) * 5) return 14;
 
 	char* local_pointer;
 
-	if(sizeof(local_pointer) != sizeof(void*)) return 13;
+	if(sizeof(local_pointer) != sizeof(void*)) return 15;
 
-	if(sizeof(*local_pointer) != 1) return 14;
+	if(sizeof(*local_pointer) != 1) return 16;
 
-	if(STRING_LENGTH != 8) return 15;
-	if(sizeof("12345") != 6) return 16;
+	if(STRING_LENGTH != 8) return 17;
+	if(sizeof("12345") != 6) return 18;
 
-	if(CHARACTER != 0x41) return 17;
-	if(CHARACTER_PLUS != 0x4B) return 18;
+	if(CHARACTER != 0x41) return 19;
+	if(CHARACTER_PLUS != 0x4B) return 20;
 }
-
-
