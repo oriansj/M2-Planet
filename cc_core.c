oriansj/M2-Planet

commit be4951f671fb6630a53c8019b750cc6ef5ccaac7

Refactor variable load out of function_call

cc_core.c
@@ -271,7 +271,7 @@ int constant_expression(void)
 }
 
 void expression(void);
-void function_call(struct token_list* s, int is_function_pointer, int is_local)
+void function_call(struct token_list* s, int is_function_pointer)
 {
 	require_match("ERROR in process_expression_list\nNo ( was found\n", "(");
 	require(NULL != global_token, "Improper function call\n");
@@ -285,6 +285,11 @@ void function_call(struct token_list* s, int is_function_pointer, int is_local)
 	emit_push(REGISTER_LOCALS, "Protect the old locals pointer");
 
 	emit_move(REGISTER_TEMP, REGISTER_STACK, "Copy new base pointer");
+
+	if(is_function_pointer)
+	{
+		emit_move(REGISTER_TEMP2, REGISTER_ZERO, "Save function pointer address");
+	}
 
 	int passed = 0;
 	while(global_token->s[0] != ')')
@@ -307,22 +312,13 @@ void function_call(struct token_list* s, int is_function_pointer, int is_local)
 		emit_push(REGISTER_RETURN, "Protect the old link register");
 	}
 
-	if(TRUE == is_function_pointer)
-	{
-		int reg = REGISTER_BASE;
-		if(is_local)
-		{
-			reg = REGISTER_LOCALS;
-		}
-
-		emit_load_relative_to_register(REGISTER_ZERO, reg, s->depth, "function pointer call");
-		emit_dereference(REGISTER_ZERO, "function pointer call");
-	}
-
 	emit_move(REGISTER_BASE, REGISTER_TEMP, "Set new base pointer");
 
 	if(TRUE == is_function_pointer)
 	{
+		emit_move(REGISTER_ZERO, REGISTER_TEMP2, "Restore function pointer");
+		emit_dereference(REGISTER_ZERO, "Deref function pointer");
+
 		if(Architecture & ARCH_FAMILY_KNIGHT)
 		{
 			emit_out("CALL R0 R15\n");
@@ -553,7 +549,7 @@ void function_load(struct token_list* a)
 	require(NULL != global_token, "incomplete function load\n");
 	if(match("(", global_token->s))
 	{
-		function_call(a, FALSE, FALSE);
+		function_call(a, FALSE);
 		return;
 	}
 
@@ -660,29 +656,32 @@ struct token_list* load_address_of_variable_into_register(int reg, char* s)
 	variable = sym_lookup(s, function->locals);
 	if(NULL != variable)
 	{
+		current_target = variable->type;
+		emit_load_relative_to_register(reg, REGISTER_LOCALS, variable->depth, "local variable load");
+
 		require(NULL != global_token, "incomplete variable load received\n");
-		if((variable->type->options & TO_FUNCTION_POINTER) && match("(", global_token->s))
+		if((current_target->options & TO_FUNCTION_POINTER) && match("(", global_token->s))
 		{
-			function_call(variable, TRUE, TRUE);
+			function_call(variable, TRUE);
 			return NULL;
 		}
 
-		current_target = variable->type;
-		emit_load_relative_to_register(reg, REGISTER_LOCALS, variable->depth, "local variable load");
 		return variable;
 	}
 
 	variable = sym_lookup(s, function->arguments);
 	if(NULL != variable)
 	{
-		require(NULL != global_token, "incomplete variable load received\n");
-		if((variable->type->options & TO_FUNCTION_POINTER) && match("(", global_token->s))
-		{
-			function_call(variable, TRUE, FALSE);
-			return NULL;
-		}
 		current_target = variable->type;
 		emit_load_relative_to_register(reg, REGISTER_BASE, variable->depth, "function argument load");
+
+		require(NULL != global_token, "incomplete variable load received\n");
+		if((current_target->options & TO_FUNCTION_POINTER) && match("(", global_token->s))
+		{
+			function_call(variable, TRUE);
+			return NULL;
+		}
+
 		return variable;
 	}
 

test/test1000/proof.answer
@@ -1,1 +1,1 @@
-be300a0d9dd7d0ab85e27178529ccc917e6fea025352a20a2fb19b2327c88519  test/test1000/proof
+8423fbb3f0046a7d11adba3a520ecd4c6644a9fce7529a28715d780c3dcf6e54  test/test1000/proof

commit f315f78d391e5c8f8fba1bcdb476798bde5b1d46

Fix indirect call clobbering function pointer on x86/amd64
function_call spilled the function pointer to REGISTER_TEMP2 (EDX/RDX)
across argument evaluation. Single-operand mul, which is emitted for
array subscripts, implicitly writes EDX:EAX, so any fp(arr[i]) or
fp(g()) where g() does array indexing ends up calling through 0.

Spill to the stack instead. Regression from be4951f.

cc_core.c
@@ -308,12 +308,15 @@ void function_call(struct token_list* s, int is_function_pointer)
 	emit_push(REGISTER_BASE, "Protect the old base pointer");
 	emit_push(REGISTER_LOCALS, "Protect the old locals pointer");
 
-	emit_move(REGISTER_TEMP, REGISTER_STACK, "Copy new base pointer");
-
+	/* Spill the function pointer to the stack instead of a register.
+	 * On x86 REGISTER_TEMP2 is EDX, which is implicitly written by mul
+	 * (used to scale array subscripts) when evaluating arguments. */
 	if(is_function_pointer)
 	{
-		emit_move(REGISTER_TEMP2, REGISTER_ZERO, "Save function pointer address");
+		emit_push(REGISTER_ZERO, "Save function pointer on stack");
 	}
+
+	emit_move(REGISTER_TEMP, REGISTER_STACK, "Copy new base pointer");
 
 	int passed = 0;
 	while(global_token->s[0] != ')')
@@ -340,7 +343,8 @@ void function_call(struct token_list* s, int is_function_pointer)
 
 	if(TRUE == is_function_pointer)
 	{
-		emit_move(REGISTER_ZERO, REGISTER_TEMP2, "Restore function pointer");
+		emit_load_relative_to_register(REGISTER_ZERO, REGISTER_TEMP, 0, "Address of saved function pointer");
+		emit_dereference(REGISTER_ZERO, "Restore function pointer from stack");
 
 		if(Architecture & ARCH_FAMILY_KNIGHT)
 		{
@@ -405,6 +409,11 @@ void function_call(struct token_list* s, int is_function_pointer)
 	if(passed > 0)
 	{
 		emit_move(REGISTER_STACK, REGISTER_BASE, "Clean up function arguments");
+	}
+
+	if(is_function_pointer)
+	{
+		emit_pop(REGISTER_ONE, "Discard saved function pointer slot");
 	}
 
 	emit_pop(REGISTER_LOCALS, "Restore old locals pointer");